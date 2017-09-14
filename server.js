f2f94647de2fecf7a464450e36873fac0ecc75b9
Christopher Smith <ctsmith2308@gmail.com>
2017-09-14 17:44:37 -0600

    get and get:id method add

---
+++ server.js
@@ -0,0 +1,45 @@
+// let express = require('express');
+// let fs = require('fs');
+// let app = express()
+// let path = require('path')
+// let filePath = path.join('pets.json')
+// // let router = express.Router();
+//
+// app.get('/pets', function(req, res) {
+//   fs.readFile('filePath', function(err, content) {
+//     if(err){console.error(err)}
+//       else{let fileData = JSON.parse(content)
+//       res.send(fileData)}
+//     })
+//   })
+
+  let express = require('express');
+  let app = express();
+  let port = process.env.port||8000;
+  let morgan = require('morgan');
+  let bodyParser = require('body-parser');
+
+  let server = require('./restfulExpressServer');
+
+
+
+  // let server = require('./server');
+
+  app.use(server);
+  // app.use(req)
+
+  app.listen(port, function(){
+    console.log("Listening on:", port);
+  })
+
+
+//   var express = require('express');
+// var router = express.Router();
+//
+// router.get('/english', function(req, res) {
+//   res.send('Hello world');
+// });
+//
+// router.get('/spanish', function(req, res) {
+//   res.send('Hola mundo');
+// });
