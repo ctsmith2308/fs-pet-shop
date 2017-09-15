fb93022cb5ba895534fb7aa0d2bfc80fb7bad20f
Christopher Smith <ctsmith2308@gmail.com>
2017-09-15 14:34:03 -0600

    add patch method

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,15 +1,3 @@
-// let express = require('express');
-// let app = express();
-// let port = process.env.port||8000;
-//
-// let server = require('./server');
-//
-// app.use(server);
-//
-// app.listen(port, function(){
-//   console.log("Listening on:", port);
-// })
-
 let express = require('express');
 let fs = require('fs');
 let path = require('path')
@@ -18,7 +6,6 @@ let router = express()
 let bodyParser = require('body-parser');
 let morgan = require('morgan');
 
-// let router = express.Router();
 
 router.use(bodyParser.json())
 
@@ -36,7 +23,7 @@ router.get('/pets', function(req, res) {
 })
 
 router.get('/pets/:id', function(req, res) {
-  fs.readFile(filePath, 'utf8', function(err, content){
+  fs.readFile(filePath, 'utf8', function(err, content) {
     if (err) {
       console.error(err.stack);
       return res.sendStatus(500);
@@ -44,15 +31,16 @@ router.get('/pets/:id', function(req, res) {
     var id = Number.parseInt(req.params.id);
     var pets = JSON.parse(content);
     if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      console.log("error in get");
       return res.sendStatus(404);
     }
-      res.set('Content-Type', 'application/json')
-      res.send(pets[id])
+    res.set('Content-Type', 'application/json')
+    res.send(pets[id])
   });
 });
 
-router.post('/pets', function (req, res){
-  fs.readFile(filePath, 'uft8', function(err, content){
+router.post('/pets', function(req, res) {
+  fs.readFile(filePath, 'uft8', function(err, content) {
     if (err) {
       console.error(err.stack)
       return res.sendStatus(500);
@@ -65,12 +53,12 @@ router.post('/pets', function (req, res){
     let petKind = req.body.kind;
     let petAge = req.body.age;
 
-    if(!petName||!petKind||!petAge){
+    if (!petName || !petKind || !petAge) {
       return res.sendStatus(400);
     }
     pets.push(body)
     let newPetJSON = JSON.stringify(pets)
-    fs.writeFile(filePath, newPetJSON, function(err){
+    fs.writeFile(filePath, newPetJSON, function(err) {
       if (err) {
         console.error(err.stack);
         return res.sendStatuscode(500)
@@ -82,31 +70,45 @@ router.post('/pets', function (req, res){
   })
 })
 
+router.patch('/pets/:id', function(req, res) {
+  fs.readFile(filePath, 'utf8', function(err, content) {
+    if (err) {
+      console.err(err);
+      return res.send(500)
+    }
+    var id = Number.parseInt(req.params.id);
+    let pets = JSON.parse(content);
 
-// router.post('/guests', function(req, res) {
-//   fs.readFile(filePath, 'utf8', function(err, content) {
-//     if (readErr) {
-//       console.error(readErr.stack);
-//       return res.sendStatus(500);
-//     }
-//     var guests = JSON.parse(guestsJSON);
-//     var guest = req.body.name;
-//     if (!guest) {
-//       return res.sendStatus(400);
-//     }
-//     guests.push(guest);
-//     var newGuestsJSON = JSON.stringify(guests);
-//     fs.writeFile(guestsPath, newGuestsJSON, function(writeErr) {
-//       if (writeErr) {
-//         console.error(writeErr.stack);
-//         return res.sendStatus(500);
-//       }
-//
-//       res.set('Content-Type', 'text/plain');
-//       res.send(guest);
-//     });
-//   });
-// });
+    let petName = req.body.name;
+    let petKind = req.body.kind;
+    let petAge = req.body.age;
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      console.log('hey im an error in patch')
+      res.sendStatus(404);
+    }
+    let pet = pets[id]
+    console.log(pet)
+    if(petName){
+      pet.name = petName
+    }
+    if(petKind){
+      pet.kind = petKind
+    }
+    if(petAge){
+      pet.age = petAge
+    }
+
+    let newPetJSON = JSON.stringify(pets)
+    fs.writeFile(filePath, newPetJSON, function(err) {
+      if (err) {
+        console.error(err.stack);
+        return res.sendStatuscode(500)
+      }
+      res.set("Content-Type", "application/json")
+      res.send(pet)
+    })
+  })
+})
 
 
-  module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-// let express = require('express');
-// let fs = require('fs');
-// let app = express()
-// let path = require('path')
-// let filePath = path.join('pets.json')
-// // let router = express.Router();
-//
-// app.get('/pets', function(req, res) {
-//   fs.readFile('filePath', function(err, content) {
-//     if(err){console.error(err)}
-//       else{let fileData = JSON.parse(content)
-//       res.send(fileData)}
-//     })
-//   })
-
   let express = require('express');
   let app = express();
   let port = process.env.port||8000;
@@ -20,13 +5,10 @@
 
   let server = require('./restfulExpressServer');
 
-
-
-  // let server = require('./server');
-
-  // app.use(server);
-  // app.use(req)
-
+  app.use(server)
+  
   app.listen(port, function(){
     console.log("Listening on:", port);
   })
+
+  module.export = app
